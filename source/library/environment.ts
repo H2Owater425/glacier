a4639e567407715382ff94e03a454ec61b7316c1
H2Owater425 <h2o@h2owr.xyz>
2023-07-14 13:47:39 +0900

    feat: Move blog configuration to environment variable

--- source/library/environment.ts
+++ source/library/environment.ts
@@ -1,16 +1,13 @@
 import 'dotenv/config';
 import { randomBytes } from 'crypto';
-import configuration from 'configuration.json';
 
-export const REQUIRED_ENVIRONMENT_VARIABLE_NAMES = ['DATABASE_URL', 'EMAIL_USER', 'EMAIL_PASSWORD', 'EMAIL_HOST', 'EMAIL_PORT', 'PORT', 'PBKDF2_ITERATION'] as const;
+export const REQUIRED_ENVIRONMENT_VARIABLE_NAMES = ['DATABASE_URL', 'EMAIL_USER', 'EMAIL_PASSWORD', 'EMAIL_HOST', 'EMAIL_PORT', 'PORT', 'PBKDF2_ITERATION', 'BLOG_NAME', 'BLOG_PASSWORD'] as const;
 
 for(let i: number = 0; i < REQUIRED_ENVIRONMENT_VARIABLE_NAMES['length']; i++) {
 	if(typeof(process['env'][REQUIRED_ENVIRONMENT_VARIABLE_NAMES[i]]) === 'undefined') {
 		throw new Error(REQUIRED_ENVIRONMENT_VARIABLE_NAMES[i] + ' must be configured');
 	}
 }
-
-process['env']['BLOG_PASSWORD'] = configuration['BLOG_PASSWORD'];
 
 process['env']['TZ'] = 'UTC';
 
